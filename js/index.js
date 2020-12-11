2d8634f0107efcc5312ecdf4769ca831500b20e5
milmalho <milmalho@iu.edu>
2020-12-10 21:08:17 -0500

    added linkedin button on sidebar

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ window.onbeforeunload = function() {
     window.scrollTo(0, 0);
 }
 
-let sidebarCollapse = document.querySelector("#sidebarCollapse");
+let sidebarCollapse = document.querySelector("#sidebar-collapse");
 let sidebar = document.querySelector("#sidebar");
 
 sidebarCollapse.addEventListener("click", function() {
-    sidebarCollapse.classList.remove("sidebarBounce");
+    sidebarCollapse.classList.remove("sidebar-bounce");
     sidebar.classList.toggle("active");
 })
