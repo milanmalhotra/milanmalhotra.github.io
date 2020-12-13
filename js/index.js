747c374df0b94e25f2fc7d91f0f2a49975412ab4
milmalho <milmalho@iu.edu>
2020-12-12 20:50:54 -0500

    sidebar links now scroll to section, added skills

--- js/index.js
+++ js/index.js
@@ -4,13 +4,19 @@ window.onbeforeunload = function() {
 
 let sidebarCollapse = document.querySelector("#sidebar-collapse");
 let sidebar = document.querySelector("#sidebar");
-let hlth360 = document.querySelector("#hlth360-fig");
-let apiprac = document.querySelector("#apipractice-fig");
-let unitygame = document.querySelector("#firstunitygame-fig");
-let drugrunners = document.querySelector("#drugrunners-fig");
-let parkingassist = document.querySelector("#parkingassist-fig");
 let discordbot = document.querySelector("#discordbot-fig");
 let figures = document.querySelectorAll("figure");
+let tabs = document.querySelectorAll(".list-group > h5");
+let home = document.querySelector(".hero-text");
+let about = document.querySelector(".about-section");
+let work = document.querySelector(".work-section");
+let projects = document.querySelector(".projects-section");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", function() {
+        scrollToSection(tab.textContent);
+    });
+});
 
 sidebarCollapse.addEventListener("click", function() {
     sidebarCollapse.classList.remove("sidebar-bounce");
@@ -27,15 +33,40 @@ function showProject(id) {
     switch (id) {
         case "hlth360-fig":
             window.open("http://2ml.7b7.myftpupload.com/milans-landing-page-1/", "_blank");
-        case "apipractice-fig":
-            break;
         case "firstunitygame-fig":
             window.open("https://milmalho.itch.io/project-breakout", "_blank");
-        case "drugrunners-fig":
+    };
+};
+
+function scrollToSection(sectionName) {
+    switch (sectionName) {
+        case "Home":
+            window.scrollTo({ top: 0, behavior: "smooth" })
             break;
-        case "parkingassist-fig":
+        case "About":
+            about.scrollIntoView({ behavior: "smooth" });
             break;
-        case "discordbot-fig":
+        case "Work":
+            work.scrollIntoView({ behavior: "smooth" });
+            break;
+        case "Projects":
+            projects.scrollIntoView({ behavior: "smooth" });
+            break;
+        case "Skills":
+            break;
+        case "Contact":
             break;
     }
+    //sidebar.classList.toggle("active");
 };
+
+//CHECKS IF ELEMENT IS IN VIEWPORT AND RETURNS TRUE OR FALSE
+// var isInViewport = function(elem) {
+//     var bounding = elem.getBoundingClientRect();
+//     return (
+//         bounding.top >= 0 &&
+//         bounding.left >= 0 &&
+//         bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// };
